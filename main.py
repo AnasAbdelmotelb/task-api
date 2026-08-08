15dd97a1efe251fef7001af1b9fcadf473f7662b
Anas Abdelmotelb Mansour <am202301500@herts.ac.uk>
2026-08-08 18:30:31 +0300

    Stage 2: Implement GET endpoints

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+tasks = [
+    {"id": 1, "title": "Buy milk", "done": False},
+    {"id": 2, "title": "Learn FastAPI", "done": True},
+    {"id": 3, "title": "Finish internship assignment", "done": False},
+]
+
+
+@app.get("/")
+def root():
+    return {
+        "name": "Task API",
+        "version": "1.0",
+        "endpoints": ["/tasks"]
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {task_id} not found"}
+    )
